d0e0b1f8e357e1b19021930adede11b586f19365
Ana Júlia <111990344+Anajukks@users.noreply.github.com>
2024-03-01 09:51:35 -0300

    Add files via upload

---
+++ src/Teste.js
@@ -0,0 +1,12 @@
+import './Teste.css';
+
+function Teste(){
+    return(
+        <div>
+            <h1 className="tit">Olá</h1>
+            <p className="paragh"><b>esse é o meu primeiro projeto React</b></p>
+        </div>
+    )
+}
+
+export default Teste;
